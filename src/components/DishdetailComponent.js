720fc4f34fc3ce5f94ad6eb07272f61e592d4335
aniketkishore100 <aniketkishore100@gmail.com>
2020-06-03 01:30:40 +0530

    added MainComponent(Container Component made menu presenComp,added datetime format in dishdetailComp

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,7 +5,7 @@ import { Card, CardBody, CardTitle, CardImg, CardText } from 'reactstrap';
 
 class DishDetail extends Component {
 
-    
+
     renderDish(select) {
         if (select != null) {
             return (
@@ -33,24 +33,24 @@ class DishDetail extends Component {
                     {
                         select.comments.map((reviews) => {
                             return (
-                                 
-        
-                                    
+
+
+
                                 <div key={reviews.id}>
-                                    
+
                                     <ul className="list-unstyled">
                                         <li className="m-2">{reviews.comment}</li>
                                         <li></li>
-                                        <p className="m-2">-- {reviews.author},{reviews.date}</p>
+                                        <p className="m-2">-- {reviews.author}, {new Intl.DateTimeFormat('en-us',{year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(reviews.date)))}</p>
                                     </ul>
                                 </div>
-        
+
                             )
-        
+
                         })
                     }
                 </div>
-                
+
             )
 
         }
@@ -64,17 +64,20 @@ class DishDetail extends Component {
 
 
     render() {
-        const select = this.props.select_dish;
+        const select = this.props.dish;
 
         return (
-            <div className="row">
+            <div className="container">
+                <div className="row">
 
-                <div className="col-12 col-md-5 m-1">
-                    {this.renderDish(select)}
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                
-                    {this.rendercomments(select)}
+                    <div className="col-12 col-md-5 m-1">
+                        {this.renderDish(select)}
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+
+                        {this.rendercomments(select)}
+                    </div>
+
                 </div>
 
             </div>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,44 +1,14 @@
 import React, { Component } from 'react';
 import { Card, CardBody, CardTitle, CardImgOverlay, CardImg, CardText } from 'reactstrap';
-import DishDetail from './DishdetailComponent';
+
 
 class Menu extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            selectedDish: null
-        }
-    }
-    onSelectDish(dish) {
-        this.setState({ selectedDish: dish })
-    }
-
-    // renderDish(dish){
-    //     if(dish != null){
-    //         return(
-    //             <Card>
-    //                 <CardImg top src={dish.image} alt={dish.name}/>
-    //                 <CardBody>
-    //                     <CardTitle>{dish.name}</CardTitle>
-    //                     <CardText>{dish.description}</CardText>
-    //                 </CardBody>
-    //             </Card>
-    //         )
-    //     }
-    //     else{
-    //         return(
-    //             <div></div>
-    //         )
-    //     }
-    // }
 
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-                <div className="col-12 col-md-5 m-1">
-                    <Card key={dish.id} onClick={() => this.onSelectDish(dish)}>
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={()=> this.props.onClick(dish.id)}>
                         <CardImg width='100%' src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -53,7 +23,6 @@ class Menu extends Component {
                 <div className="row">
                     {menu};
                 </div>
-                <DishDetail select_dish = {this.state.selectedDish}/>             
             </div>
         );
     }
