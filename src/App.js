de0f0c3a8324c41ce57c44d58b2db94ff3c2deff
aniketkishore100 <aniketkishore100@gmail.com>
2020-06-11 01:19:32 +0530

    added redux files and connected main to redux configureStore

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React , {Component}from 'react';
 import './App.css';
 import Main from './components/MainComponent';
 import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux'
+// Provider is used to join the redux store to react
+import {ConfigureStore} from './redux/configureStore'
+
+const store = ConfigureStore();
 
 class App extends Component {
   
   render(){
     return (
-      <BrowserRouter>
+      <Provider store ={store}>  
+        <BrowserRouter>
       <div className="App">
         <Main />
       </div>
       </BrowserRouter>
+      </Provider>
+      
       
     );
   }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,37 +6,38 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
+import { Switch, Route, Redirect, withRouter} from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state =>{
+    return{
+        dishes:state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+    }
+}
 
 class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            dishes: DISHES,
-            leaders: LEADERS,
-            promotions: PROMOTIONS,
-            comments: COMMENTS
-        };
+
     }
 
 
     render() {
         const HomePage = () => {
             return (
-                <Home dish={this.state.dishes.filter((dish) => dish.featured === true)[0]}
-                    leader={this.state.leaders.filter((leader) => leader.featured === true)[0]}
-                    promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
+                <Home dish={this.props.dishes.filter((dish) => dish.featured === true)[0]}
+                    leader={this.props.leaders.filter((leader) => leader.featured === true)[0]}
+                    promotion={this.props.promotions.filter((promotion) => promotion.featured)[0]}
                 />)
         }
 
         const DishWithId = ({ match }) => {
             return (
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
-                    comment={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+                <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comment={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
             )
         }
 
@@ -47,14 +48,14 @@ class Main extends Component {
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => {
                         return (
-                            <Menu dishes={this.state.dishes} />
+                            <Menu dishes={this.props.dishes} />
                         )
                     }} />
                     <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />} />
                     <Route exact path='/aboutus' component={()=>{
                         return(
-                            <About leaders ={this.state.leaders}/>
+                            <About leaders ={this.props.leaders}/>
                         )
                     }}/>
                     <Redirect to='/home' />
@@ -66,4 +67,4 @@ class Main extends Component {
 
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
